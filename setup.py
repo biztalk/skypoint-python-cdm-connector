ff3e1c5acde9f3c15228a380274b9af1bcc12d14
Gautam <gautam@skypointcloud.com>
2020-03-27 12:43:38 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name="cdm-connector",
-    version="0.0.2",
+    version="0.0.3",
     description="A Python package to read and write files in CDM format. Customized for SkyPoint use cases.",
     long_description=readme(),
     long_description_content_type="text/markdown",
